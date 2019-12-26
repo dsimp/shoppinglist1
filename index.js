bab705632b750e6e9b8490a8b0d5cae41a74f8d2
dsimp <55302995+dsimp@users.noreply.github.com>
2019-12-26 12:21:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-function lineItem(){
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(e){
-        e.preventDefault();
-        $(e.target).closest('li').find('.shopping-item').toggleClass('.shopping-item_checked');
+$(function() {
+  $('#js-shopping-list-form').submit(event => {
+     event.preventDefault();
+     
+     const item = $(this).find(
+      'input[name="shopping-list-entry"]').val();
+       
+    const buttonDiv = $(this).find(
+      ".shopping-item-controls").html();
+    
+    $("ul").append(
+      "<li>" +
+      '<span class = "shopping-item">' +
+      [item] +
+      "</span>" +
+      '<div class = "shopping-item-controls">'+
+      '<button class="shopping-item-toggle">' +
+      '<span class="button-label">check</span>' +
+      '</button>' + 
+      '<button class="shopping-item-delete">' +
+      '<span class="button-label">delete</span>' +
+      '</button>' +
+      "</div>" +
+      "</li>");   
 
-    });
-}
+     $(".js-shopping-list-entry").val(''); 
+    
+  });
 
+  $('.shopping-list').on('click','.shopping-item-toggle',function(event) {
+    const targetItem = $(event.currentTarget).closest("li");
+    targetItem.find(".shopping-item").toggleClass('shopping-item__checked');
+  });
+ 
+  $('.shopping-list').on('click','.shopping-item-delete',function(event) {
+    const delItem = $(event.currentTarget).closest("li");
+    delItem.remove();
+  });
+});
 
-
-function deleteItem(){
-    $('.shopping-list').on('click', '.shopping-item-delete', function(e){
-        e.preventDefault();
-        $(e.target).closest('li').remove();
-
-    })
-}
-
-function makeShoppingList(){
-    $('#js-shopping-list-form').on('submit', function(e){
-        e.preventDefault();
-
-        const value = $('#shopping-list-entry').val();
-        console.log(value);
-
-        $('.shopping-list').append($(origList(value)));
-        $('shopping-list-entry').val('');
-    });
-};
-
-$(makeShoppingList)
-$(lineItem)
-$(deleteItem)
